375f40540dda51dc07cbfa00949ad0c26051deca
obrunofontana <obrunofontana@gmail.com>
2023-02-24 21:10:49 -0300

    chore: altera tempo de expiracao

--- config/default.ts
+++ config/default.ts
@@ -1,7 +1,7 @@
 export default {
   origin: 'http://localhost:5173',
   accessTokenExpiresIn: 21600, // 15 dias
-  refreshTokenExpiresIn: 60,
-  redisCacheExpiresIn: 60,
+  refreshTokenExpiresIn: 240,
+  redisCacheExpiresIn: 240,
   emailFrom: 'obrunofontana@gmail.com',
 };
